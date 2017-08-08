278775941b1b2be473467114f94b7ab16fbf0e4b
qqyemingqq <qqyemingqq@gmail.com>
2017-08-08 08:45:36 +0800

    edit

---
+++ compareoperate.cpp
@@ -0,0 +1,6 @@
+#include "comepareoperate.h"
+
+comepareoperate::comepareoperate()
+{
+
+}
---
+++ compareoperate.h
@@ -0,0 +1,12 @@
+#ifndef COMEPAREOPERATE_H
+#define COMEPAREOPERATE_H
+
+#include <QWidget>
+
+class comepareoperate
+{
+public:
+    comepareoperate();
+};
+
+#endif // COMEPAREOPERATE_H
