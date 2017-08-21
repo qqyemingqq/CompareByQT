e36272f5c0b77649998775f29529f27ac53a2c5b
qqyemingqq <qqyemingqq@gmail.com>
2017-08-21 09:44:34 +0800

    重命名

---
+++ comparetables.cpp
@@ -0,0 +1,6 @@
+#include "comparetables.h"
+
+CompareTables::CompareTables()
+{
+
+}
---
+++ comparetables.h
@@ -0,0 +1,11 @@
+#ifndef COMPARETABLES_H
+#define COMPARETABLES_H
+
+
+class CompareTables
+{
+public:
+    CompareTables();
+};
+
+#endif // COMPARETABLES_H
